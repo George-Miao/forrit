2568bdaa1362e4dbbeaf708f8c992894bad7bb0e
George Miao <gm@miao.dev>
2022-10-19 05:12:33 -0400

    feat: api

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,15 @@
 #![feature(iter_intersperse)]
 
-use std::fs;
-
 use color_eyre::Result;
-use rss::Channel;
+use ejdb::{
+    bson::{from_bson, ordered::OrderedDocument},
+    query::{Q, QH},
+};
+use tracing::info;
 
 use crate::{
-    bangumi_moe::{Current, ID},
-    model::Subscribed,
+    bangumi_moe::v2::{Tag, WithId},
+    model::WithOId,
 };
 
 mod api;
@@ -19,33 +21,64 @@ async fn main() -> Result<()> {
     tracing_subscriber::fmt::init();
     color_eyre::install()?;
 
+    run().await?;
+
+    Ok(())
+}
+
+async fn run() -> Result<()> {
+    // let bgm = fs::read("data/bangumi.current.v2.json")?;
+    // let bgm: Current = serde_json::from_slice(&bgm)?;
+
+    // let mut tag_set = HashSet::new();
+    // let teams_tags = bgm
+    //     .working_teams
+    //     .into_values()
+    //     .flat_map(|x| x.into_iter())
+    //     .map(|x| x.unwrap().tag);
+    // let all_tags = bgm
+    //     .bangumis
+    //     .into_iter()
+    //     .map(|x| x.unwrap().tag)
+    //     .chain(teams_tags)
+    //     .filter(|t| {
+    //         if tag_set.contains(&t.id) {
+    //             false
+    //         } else {
+    //             tag_set.insert(t.id.clone());
+    //             true
+    //         }
+    //     })
+    //     .map(|x| match ejdb::bson::to_bson(&x.unwrap()).unwrap() {
+    //         ejdb::bson::Bson::Document(doc) => doc,
+    //         _ => unreachable!(),
+    //     });
+    // info!("all_tags: {all_tags:#?}");
+    // assert_bson_iter(&all_tags);
     // let bgm = reqwest::get("https://bangumi.moe/api/bangumi/current")
     //     .await?
     //     .json::<Vec<Bangumi>>()
     //     .await?;
 
-    let bgm = fs::read("data/bangumi.current.v2.json")?;
-    let bgm: Current = serde_json::from_slice(&bgm)?;
+    let db = ejdb::Database::open("data/main.db")?;
 
-    let example = bgm
-        .bangumis
-        .into_iter()
-        .find(|x| x.tag_id == ID("62c6ce11f248710007e811b1".into()))
-        .unwrap();
-    let team = bgm.working_teams[&example.tag_id.0][0].clone();
+    let col = db.collection("tags")?;
 
-    let sub = Subscribed {
-        tags: vec![team.tag_id],
-        bangumi_tag: example.tag,
-        season: None,
-    };
+    for i in col
+        .query(Q.field("type").eq("bangumi"), QH.empty())
+        .find()?
+        .map(|x| x.map(|x: OrderedDocument| -> WithOId<Tag> { from_bson(x.into()).unwrap() }))
+    {
+        info!("i: {i:#?}");
+    }
 
-    let rss = sub.rss_url_v1()?;
+    // col.save_all(all_tags)?;
 
-    let rss_content = reqwest::get(rss).await?.bytes().await?;
-    let c = Channel::read_from(&rss_content[..])?;
+    // for i in col.query(Q.slice("", i64::MAX), QH.empty()).find()? {
+    //     let t: Tag = from_bson(i?.into())?;
+    //     info!("{:#?}", t);
+    // }
 
-    println!("{:#?}", c);
-
+    // info!("{:#?}", res);
     Ok(())
 }
--- src/model.rs
+++ src/model.rs
@@ -1,29 +1,104 @@
+use std::ops::{Deref, DerefMut};
+
 use color_eyre::Result;
+use ejdb::bson::oid::ObjectId;
+use reqwest::Url;
 use serde::{Deserialize, Serialize};
 
-use crate::bangumi_moe::{Tag, ID};
+use crate::bangumi_moe::v2::{Id, Tag, WithId};
 
-#[derive(Clone, Debug, PartialEq, Eq, Serialize, Deserialize)]
-pub struct Subscribed {
-    pub tags: Vec<ID>,
-    pub bangumi_tag: Tag,
+#[derive(Clone, Debug, Hash, PartialEq, Eq, Serialize, Deserialize)]
+pub struct Subscription {
+    pub tags: Vec<Id>,
+    pub bangumi_tag: WithId<Tag>,
     pub season: Option<u16>,
+    pub include_pattern: Option<String>,
+    pub exclude_pattern: Option<String>,
 }
 
-impl Subscribed {
-    pub fn rss_url_v1(&self) -> Result<url::Url> {
+impl Subscription {
+    pub fn rss_url(&self) -> Result<Url> {
         let tags: String = self
             .tags
             .iter()
             .chain(std::iter::once(&self.bangumi_tag.id))
-            .map(|x| x.0.as_str())
-            .intersperse("+")
+            .map(|x| x.to_string())
+            .intersperse("+".to_owned())
             .collect();
 
-        url::Url::parse(&format!("https://bangumi.moe/rss/tags/{tags}")).map_err(Into::into)
+        Url::parse(&format!("https://bangumi.moe/rss/tags/{tags}")).map_err(Into::into)
     }
 
-    pub fn add_filter(&mut self, tag: ID) {
+    pub fn with_filter(mut self, tag: Id) -> Self {
         self.tags.push(tag);
+        self
     }
+}
+
+#[derive(Clone, Debug, Hash, PartialEq, Eq, PartialOrd, Ord, Serialize, Deserialize)]
+pub struct WithOId<T> {
+    #[serde(rename = "_id")]
+    pub id: ObjectId,
+    #[serde(flatten)]
+    pub data: T,
+}
+
+impl<T> Deref for WithOId<T> {
+    type Target = T;
+
+    fn deref(&self) -> &Self::Target {
+        &self.data
+    }
+}
+
+impl<T> DerefMut for WithOId<T> {
+    fn deref_mut(&mut self) -> &mut Self::Target {
+        &mut self.data
+    }
+}
+
+impl<T> WithOId<T> {
+    pub fn unwrap(self) -> T {
+        self.data
+    }
+}
+
+#[tokio::test]
+async fn test_rss_url() -> Result<()> {
+    use std::fs;
+
+    use crate::{api::Api, bangumi_moe::v2::Current};
+
+    let bgm = fs::read("data/bangumi.current.v2.json")?;
+    let bgm: Current = serde_json::from_slice(&bgm)?;
+
+    let example = bgm
+        .bangumis
+        .into_iter()
+        .find(|x| x.tag_id.deref() == "62c6ce11f248710007e811b1")
+        .unwrap()
+        .unwrap();
+
+    let team = bgm.working_teams[&example.tag_id][0].clone().unwrap();
+
+    let sub = Subscription {
+        tags: vec![team.tag_id],
+        bangumi_tag: example.tag,
+        season: None,
+        include_pattern: None,
+        exclude_pattern: None,
+    };
+
+    let c = Api::new();
+    let res = c.retreive_rss(&sub).await?;
+    println!("res: {:#?}", res);
+    for item in res
+        .items
+        .into_iter()
+        .filter(|x| x.enclosure.is_some() && x.title.is_some())
+    {
+        println!("Name: {}", item.title.unwrap());
+        println!("Magnet: {}", item.enclosure.unwrap().url);
+    }
+    Ok(())
 }
