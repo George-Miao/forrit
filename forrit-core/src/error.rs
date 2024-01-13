7764358bcb0da2a443f026fb53b5784ab6d3e8ec
George Miao <gm@miao.dev>
2024-01-13 16:25:46 +0800

    feat: boom

--- forrit-core/src/error.rs
+++
@@ -1,9 +0,0 @@
-use thiserror::Error;
-
-#[derive(Error, Debug, Clone, PartialEq, Eq)]
-pub enum Error {
-    #[error("Url parse error: {0}")]
-    UrlParseError(#[from] url::ParseError),
-}
-
-pub type Result<T> = std::result::Result<T, Error>;
--- forrit-web/app/page.tsx
+++
@@ -1,8 +0,0 @@
-export default function Home() {
-  return (
-    <>
-      <h1>123123</h1>
-      <div className="h-[200vh]"></div>
-    </>
-  )
-}
