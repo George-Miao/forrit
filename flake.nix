39f937c5bca74ac30a76fd32f6c187077bcebf25
George Miao <gm@miao.dev>
2024-09-11 16:59:38 -0400

    fix: remove duplicate nixpkgs in flake

--- flake.nix
+++ flake.nix
@@ -4,8 +4,11 @@
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     flake-utils.url = "github:numtide/flake-utils";
-    rust-overlay.url = "github:oxalica/rust-overlay";
     crane.url = "github:ipetkov/crane";
+    rust-overlay = {
+      url = "github:oxalica/rust-overlay";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
   outputs = {
